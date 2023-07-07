bfa0680f4cab1b62490e6d51c3f4242597ec4d26
Rafal Pawelec <pawelec.rafal@gmail.com>
2023-07-07 19:51:23 +0200

    DateFormatter, TransactionRow, SwiftUIFontIcon

--- Expense Tracker/Expense Tracker/TransactionRow.swift
+++ Expense Tracker/Expense Tracker/TransactionRow.swift
@@ -10,12 +10,50 @@ import SwiftUI
 struct TransactionRow: View {
     var transaction: Transaction
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+
+        HStack(spacing: 20) {
+            
+            // MARK: Transaction Category Type
+            
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(Color.icon.opacity(0.3))
+                .frame(width: 44, height: 44) 
+            
+            
+            VStack(alignment: .leading,spacing: 6) {
+                //MARK: Transacion merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+                
+                //MARK: Transaction Category
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(1)
+                
+                //MARK: Transaction Date
+                Text(transaction.dateParsed, format: .dateTime.year().month().day())
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+                    .lineLimit(1)
+            }
+            
+            Spacer()
+            
+            //MARK: Transaction Amount
+            Text(transaction.signedAmount, format: .currency(code: "PLN"))
+                .bold()
+                .foregroundColor(transaction.type == TransactionType.credit.rawValue ? Color.text : .primary )
+        }
+        .padding([.top, .bottom], 8)
     }
 }
 
 struct TransactionRow_Previews: PreviewProvider {
     static var previews: some View {
-        TransactionRow()
+        TransactionRow(transaction: transactionPreviewData)
     }
 }
