c4ae5681692655823c44a48bce445f1723fb0b2c
Rafal Pawelec <pawelec.rafal@gmail.com>
2023-07-07 21:57:24 +0200

    Recent Transactions

--- Expense Tracker/Expense Tracker/ContentView.swift
+++ Expense Tracker/Expense Tracker/ContentView.swift
@@ -22,6 +22,8 @@ struct ContentView: View {
                 .padding()
                 .frame(maxWidth: .infinity)
                 
+                
+                
             }
             .background(Color.background)
             .navigationBarTitleDisplayMode(.inline)
