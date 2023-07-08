007e8f0861cafc751d37d9626b72c32dc8ee3e69
Rafal Pawelec <pawelec.rafal@gmail.com>
2023-07-08 11:14:17 +0200

    Line Chart init

--- Expense Tracker/Expense Tracker/ContentView.swift
+++ Expense Tracker/Expense Tracker/ContentView.swift
@@ -6,17 +6,34 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    var demoData: [Double] = [9,3,5,12,3,24]
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 24) {
-                    // Title
+                    //MARK: Title
                     Text("Overview")
                         .font(.title)
                         .bold()
                     
+                    //MARK: Chart
+                    CardView {
+                        VStack {
+                            ChartLabel("$900", type: .title)
+                            LineChart()
+                        }
+                        .background(Color.systemBackground)       
+                    }
+                    .data(demoData)
+                    .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(4), Color.icon)))
+                    .frame(height: 300)
+
+
+                    
+                    //MARK: Recent Transaction List
                     RecentTransactionList()
                     
                 }
